2f3d1f5c59d2b6a5c96b31f59687f2bfbc6ea4b8
TashafMahmood <tashaf@elred.io>
2024-06-15 10:12:04 +0530

    changes input field testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>
-      First react test case
+    <div className="m-10 text-center">
+      First react test case <br />
+      <br />
+      <h1 className="text-lg font-bold mb-4">Input Box Testing</h1>
+      <input
+        placeholder="Enter username"
+        className="bg-slate-300 px-4 py-2 rounded-md"
+        name="input-field"
+        id="myinput"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,14 @@ test('testing app',()=>{
   const text = screen.getByText("First react test case")
   expect(text).toBeInTheDocument()
 })
+
+// Testing Input Box
+test("testing input box",()=>{
+  render(<App/>)
+  const checkInputField = screen.getByRole("textbox")
+  const checkInputPlaceholder = screen.getByPlaceholderText("Enter username")
+  expect(checkInputField).toBeInTheDocument()
+  expect(checkInputPlaceholder).toBeInTheDocument()
+  expect(checkInputField).toHaveAttribute("name","input-field")
+  expect(checkInputField).toHaveAttribute("id","myinput")
+})
