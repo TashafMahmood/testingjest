0431725cc4be4027ea8dbf0cab9c228e735f5557
TashafMahmood <tashaf@elred.io>
2024-06-15 10:45:34 +0530

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,21 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 
-describe("UI TESTING GROUP", () => {
+describe.only("UI TESTING GROUP", () => {
+  test("test case 1", () => {
+    render(<App />);
+    const checkInputField = screen.getByRole("textbox");
+    expect(checkInputField).toBeInTheDocument();
+  });
+
+  test("test case 2", () => {
+    render(<App />);
+    const checkInputPlaceholder = screen.getByPlaceholderText("Enter username");
+    expect(checkInputPlaceholder).toBeInTheDocument();
+  });
+});
+
+describe.skip("UI TESTING GROUP", () => {
   test("test case 1", () => {
     render(<App />);
     const checkInputField = screen.getByRole("textbox");
